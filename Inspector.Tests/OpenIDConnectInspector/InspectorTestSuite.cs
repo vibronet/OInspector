8cf3d5a63f9317a0b564c772d163034be941f891
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2015-03-21 15:48:03 -0700

    QA: Initial check-in of the plugin's unit test project

---
+++ Inspector.Tests/OpenIDConnectInspector/InspectorTestSuite.cs
@@ -0,0 +1,14 @@
+﻿namespace OpenIDConnect.Inspector.Tests.OpenIDConnectInspector
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class InspectorTestSuite
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

ede966d69223dd603bb03cd6662133fa68cd7812
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2015-04-23 00:35:03 -0700

    Inspector: Removed unused 'Body' section of 'WebFormEditor' control to expand visible/scrollable area and aid usability

--- Inspector/Controls/WebFormEditor.Designer.cs
+++ Inspector/Controls/WebFormEditor.Designer.cs
@@ -7,12 +7,9 @@
 
     partial class WebFormEditor
     {
-        internal DataGridView gridBody;
         internal DataGridView gridQueryString;
-        internal Label lblBody;
         internal Label lblQueryString;
         private ContextMenuStrip mnuContext;
-        internal Panel pnlBody;
         internal Panel pnlHeaders;
         private Splitter splitHeadersBody;
         private ToolStripMenuItem tsmiCopyCell;
@@ -44,115 +41,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new Container();
-            this.splitHeadersBody = new Splitter();
-            this.pnlHeaders = new Panel();
-            this.gridQueryString = new DataGridView();
-            this.mnuContext = new ContextMenuStrip(this.components);
-            this.tsmiCopyCell = new ToolStripMenuItem();
-            this.tsmiSendCellToTextWizard = new ToolStripMenuItem();
-            this.lblQueryString = new Label();
-            this.pnlBody = new Panel();
-            this.gridBody = new DataGridView();
-            this.lblBody = new Label();
+            this.components = new System.ComponentModel.Container();
+            this.splitHeadersBody = new System.Windows.Forms.Splitter();
+            this.pnlHeaders = new System.Windows.Forms.Panel();
+            this.gridQueryString = new System.Windows.Forms.DataGridView();
+            this.mnuContext = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsmiCopyCell = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiSendCellToTextWizard = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblQueryString = new System.Windows.Forms.Label();
             this.pnlHeaders.SuspendLayout();
-            ((ISupportInitialize)this.gridQueryString).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridQueryString)).BeginInit();
             this.mnuContext.SuspendLayout();
-            this.pnlBody.SuspendLayout();
-            ((ISupportInitialize)this.gridBody).BeginInit();
-            base.SuspendLayout();
-            this.splitHeadersBody.Dock = DockStyle.Bottom;
-            this.splitHeadersBody.Location = new Point(0, 0x85);
+            this.SuspendLayout();
+            // 
+            // splitHeadersBody
+            // 
+            this.splitHeadersBody.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.splitHeadersBody.Location = new System.Drawing.Point(0, 277);
             this.splitHeadersBody.Name = "splitHeadersBody";
-            this.splitHeadersBody.Size = new Size(0x231, 3);
+            this.splitHeadersBody.Size = new System.Drawing.Size(561, 3);
             this.splitHeadersBody.TabIndex = 3;
             this.splitHeadersBody.TabStop = false;
+            // 
+            // pnlHeaders
+            // 
             this.pnlHeaders.Controls.Add(this.gridQueryString);
             this.pnlHeaders.Controls.Add(this.lblQueryString);
-            this.pnlHeaders.Dock = DockStyle.Fill;
-            this.pnlHeaders.Location = new Point(0, 0);
+            this.pnlHeaders.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlHeaders.Location = new System.Drawing.Point(0, 0);
             this.pnlHeaders.Name = "pnlHeaders";
-            this.pnlHeaders.Size = new Size(0x231, 0x85);
+            this.pnlHeaders.Size = new System.Drawing.Size(561, 277);
             this.pnlHeaders.TabIndex = 6;
+            // 
+            // gridQueryString
+            // 
             this.gridQueryString.AllowUserToAddRows = false;
             this.gridQueryString.AllowUserToDeleteRows = false;
             this.gridQueryString.AllowUserToResizeRows = false;
-            this.gridQueryString.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            this.gridQueryString.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
-            this.gridQueryString.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
-            this.gridQueryString.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridQueryString.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.gridQueryString.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.gridQueryString.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.gridQueryString.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.gridQueryString.ContextMenuStrip = this.mnuContext;
-            this.gridQueryString.Dock = DockStyle.Fill;
-            this.gridQueryString.EditMode = DataGridViewEditMode.EditOnEnter;
+            this.gridQueryString.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridQueryString.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
             this.gridQueryString.EnableHeadersVisualStyles = false;
-            this.gridQueryString.Location = new Point(0, 13);
+            this.gridQueryString.Location = new System.Drawing.Point(0, 13);
             this.gridQueryString.Name = "gridQueryString";
             this.gridQueryString.ReadOnly = true;
-            this.gridQueryString.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
-            this.gridQueryString.Size = new Size(0x231, 120);
+            this.gridQueryString.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.gridQueryString.Size = new System.Drawing.Size(561, 264);
             this.gridQueryString.TabIndex = 5;
-            this.mnuContext.Items.AddRange(new ToolStripItem[] { this.tsmiCopyCell, this.tsmiSendCellToTextWizard });
+            // 
+            // mnuContext
+            // 
+            this.mnuContext.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiCopyCell,
+            this.tsmiSendCellToTextWizard});
             this.mnuContext.Name = "contextMenuStrip1";
             this.mnuContext.ShowImageMargin = false;
-            this.mnuContext.Size = new Size(160, 0x30);
-            this.mnuContext.Opening += new CancelEventHandler(this.mnuContext_Opening);
+            this.mnuContext.Size = new System.Drawing.Size(160, 48);
+            this.mnuContext.Opening += new System.ComponentModel.CancelEventHandler(this.mnuContext_Opening);
+            // 
+            // tsmiCopyCell
+            // 
             this.tsmiCopyCell.Name = "tsmiCopyCell";
             this.tsmiCopyCell.ShortcutKeyDisplayString = "Ctrl+C";
-            this.tsmiCopyCell.Size = new Size(0xb8, 0x16);
+            this.tsmiCopyCell.Size = new System.Drawing.Size(159, 22);
             this.tsmiCopyCell.Text = "&Copy";
-            this.tsmiCopyCell.Click += new EventHandler(this.tsmiCopyCell_Click);
+            this.tsmiCopyCell.Click += new System.EventHandler(this.tsmiCopyCell_Click);
+            // 
+            // tsmiSendCellToTextWizard
+            // 
             this.tsmiSendCellToTextWizard.Name = "tsmiSendCellToTextWizard";
-            this.tsmiSendCellToTextWizard.Size = new Size(0xb8, 0x16);
+            this.tsmiSendCellToTextWizard.Size = new System.Drawing.Size(159, 22);
             this.tsmiSendCellToTextWizard.Text = "S&end to TextWizard...";
-            this.tsmiSendCellToTextWizard.Click += new EventHandler(this.tsmiSendCellToTextWizard_Click);
-            this.lblQueryString.Dock = DockStyle.Top;
-            this.lblQueryString.Location = new Point(0, 0);
+            this.tsmiSendCellToTextWizard.Click += new System.EventHandler(this.tsmiSendCellToTextWizard_Click);
+            // 
+            // lblQueryString
+            // 
+            this.lblQueryString.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblQueryString.Location = new System.Drawing.Point(0, 0);
             this.lblQueryString.Name = "lblQueryString";
-            this.lblQueryString.Size = new Size(0x231, 13);
+            this.lblQueryString.Size = new System.Drawing.Size(561, 13);
             this.lblQueryString.TabIndex = 8;
-            this.lblQueryString.Text = "QueryString";
-            this.pnlBody.Controls.Add(this.gridBody);
-            this.pnlBody.Controls.Add(this.lblBody);
-            this.pnlBody.Dock = DockStyle.Bottom;
-            this.pnlBody.Location = new Point(0, 0x88);
-            this.pnlBody.Name = "pnlBody";
-            this.pnlBody.Size = new Size(0x231, 0x90);
-            this.pnlBody.TabIndex = 7;
-            this.gridBody.AllowUserToAddRows = false;
-            this.gridBody.AllowUserToDeleteRows = false;
-            this.gridBody.AllowUserToResizeRows = false;
-            this.gridBody.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            this.gridBody.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
-            this.gridBody.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
-            this.gridBody.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gridBody.ContextMenuStrip = this.mnuContext;
-            this.gridBody.Dock = DockStyle.Fill;
-            this.gridBody.EditMode = DataGridViewEditMode.EditOnEnter;
-            this.gridBody.EnableHeadersVisualStyles = false;
-            this.gridBody.Location = new Point(0, 13);
-            this.gridBody.Name = "gridBody";
-            this.gridBody.ReadOnly = true;
-            this.gridBody.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
-            this.gridBody.Size = new Size(0x231, 0x83);
-            this.gridBody.TabIndex = 6;
-            this.lblBody.Dock = DockStyle.Top;
-            this.lblBody.Location = new Point(0, 0);
-            this.lblBody.Name = "lblBody";
-            this.lblBody.Size = new Size(0x231, 13);
-            this.lblBody.TabIndex = 9;
-            this.lblBody.Text = "Body";
-            base.Controls.Add(this.pnlHeaders);
-            base.Controls.Add(this.splitHeadersBody);
-            base.Controls.Add(this.pnlBody);
-            this.Font = new Font("Tahoma", 8.25f);
-            base.Name = "WebFormEditor";
-            base.Size = new Size(0x231, 280);
+            this.lblQueryString.Text = "Context";
+            // 
+            // WebFormEditor
+            // 
+            this.Controls.Add(this.pnlHeaders);
+            this.Controls.Add(this.splitHeadersBody);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F);
+            this.Name = "WebFormEditor";
+            this.Size = new System.Drawing.Size(561, 280);
             this.pnlHeaders.ResumeLayout(false);
-            ((ISupportInitialize)this.gridQueryString).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridQueryString)).EndInit();
             this.mnuContext.ResumeLayout(false);
-            this.pnlBody.ResumeLayout(false);
-            ((ISupportInitialize)this.gridBody).EndInit();
-            base.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Inspector/Controls/WebFormEditor.cs
+++ Inspector/Controls/WebFormEditor.cs
@@ -16,19 +16,9 @@
             this.gridQueryString.Columns[0].Name = "Name";
             this.gridQueryString.Columns[0].MinimumWidth = 40;
             this.gridQueryString.Columns[1].Name = "Value";
-            this.gridBody.BackgroundColor = Fiddler.CONFIG.colorDisabledEdit;
-            this.gridBody.ColumnCount = 2;
-            this.gridBody.Columns[0].Name = "Name";
-            this.gridBody.Columns[0].MinimumWidth = 40;
-            this.gridBody.Columns[1].Name = "Value";
-            this.gridBody.RowHeadersWidth = 0x19;
             this.gridQueryString.RowHeadersWidth = 0x19;
-            this.gridBody.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
             this.gridQueryString.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
-            if (this.gridBody.Font.Size != Fiddler.CONFIG.flFontSize)
-            {
-                this.gridBody.Font = new Font(this.gridBody.Font.FontFamily, Fiddler.CONFIG.flFontSize);
-            }
+
             if (this.gridQueryString.Font.Size != Fiddler.CONFIG.flFontSize)
             {
                 this.gridQueryString.Font = new Font(this.gridQueryString.Font.FontFamily, Fiddler.CONFIG.flFontSize);
@@ -71,32 +61,6 @@
             if (!string.IsNullOrEmpty(str))
             {
                 Fiddler.FiddlerApplication.UI.actShowTextWizard(str);
-            }
-        }
-
-        public bool BodyIsReadOnly
-        {
-            get
-            {
-                return this.gridBody.ReadOnly;
-            }
-            set
-            {
-                this.gridBody.ReadOnly = value;
-                this.gridBody.RowHeadersVisible = this.gridBody.AllowUserToAddRows = this.gridBody.AllowUserToDeleteRows = !value;
-            }
-        }
-
-        public bool QueryStringIsReadOnly
-        {
-            get
-            {
-                return this.gridQueryString.ReadOnly;
-            }
-            set
-            {
-                this.gridQueryString.ReadOnly = value;
-                this.gridQueryString.RowHeadersVisible = this.gridQueryString.AllowUserToAddRows = this.gridQueryString.AllowUserToDeleteRows = !value;
             }
         }
 
